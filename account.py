37dc749c31bad67bc189d0aa5b7179f3981391ae
alligatorme <alligatorme@gmail.com>
2017-09-26 17:32:47 +0800

    clean 2017-09-26 17:30

--- account.py
+++ account.py
@@ -43,7 +43,7 @@ class contract(metaclass=singleton):
         '''
             p:price|n:amount|rto:margin ratio|tax:fee|npr:amount per contract|drt:direction-open/short|prf:profit|mrg:margin|fri:fixed profit
         '''
-        # self.tank={}
+
         self.tank=deque()
         self.fri=0
         self.pst=0
@@ -52,8 +52,7 @@ class contract(metaclass=singleton):
         
         self.npr,self.tax,self.rto=get_info(self.parse_name(name))
 
-        # self.name=name
-        # self.drt=(name[-1:]=='-') and -1 or 1
+
         self.get_prf=lambda n,p:n*(self.drt*self.npr*p-self.tax)
         self.get_mrg=lambda n,p:n*p*self.rto*self.npr/100    
 
@@ -63,13 +62,6 @@ class contract(metaclass=singleton):
         self.drt=(name[-1:]=='-') and -1 or 1
         return name[:-1]
 
-    # def refresh(self,prs):
-    #     # self.mrg=0
-    #     self.prf=0
-    #     for _,n,p in self.tank:
-    #         self.prf+=self.get_prf(n,prs-p)
-    #         # self.mrg+=self.get_mrg(n,prs)
-
     def lump(self):
         cum=cnt=0
         for _,n,p in self.tank:
@@ -77,23 +69,19 @@ class contract(metaclass=singleton):
             cnt+=n
         self.pst=cnt
         self.avgp=cnt and cum/cnt or 0
-        # return cnt and cum/cnt or 0
 
     def ocsc(self,idt,t,n,p):        
         if idt==self.drt:
             self.tank.append((t,n,p))
         else:
-            # n1=n
             while (n>0 and len(self.tank)>0):
                 t0,n0,p0=self.tank.pop()
                 self.fri+=self.get_prf(min(n,n0),p-p0)
                 n-=n0
                 if n<0: self.tank.append((t0,-n,p0))
-        # self.pst+=idt*self.drt*n
         self.lump()
     
     def divide(self,src,idx):
-        # div=(src.get_loc(i) for i in idx)
         div=(np.where(src==i) for i in idx)
         start=0
         for ([i],) in div:
@@ -116,7 +104,6 @@ class contract(metaclass=singleton):
             
             self.ocsc(sign_iter[0],src.idx[i],1,src.src[i,3])
             sign_iter.iternext()
-            # self.ocsc(sign_iter.__next__(),src.idx[i],1,src.src[i,3])
 
 dname=os.path.split(os.path.realpath(__file__))[0]+'\me.s3db'
 def get_info(name):    
@@ -140,9 +127,7 @@ def shift(rst,n):
 @elapse
 def cross(rst):    
     rst=np.sign(rst)
-    # print(rst)
     sft=shift(rst,1)
-    # sft=rst.__rshift__(1)
     zr=rst==0
     if any(zr): 
         print('zero appears =',len(zr[zr]))
@@ -150,8 +135,6 @@ def cross(rst):
     sft=shift(rst,1) 
     mrk=~np.logical_or(sft==rst,np.isnan(sft))
     mrk=np.ma.array(rst,mask=~mrk)
-    # mrk=np.where(mrk)
-    # print(mrk)
     return mrk
 
 
@@ -179,8 +162,6 @@ def main02():
    
     m15=source(db.data['m1505'].values,db.data['m1505'].index.values)
     idx=cross(stg(m15.src.T[3]))
-    # print(signal.mask)
-    # print(df.index[~idx.mask][1:])
     # idx=pd.DataFrame(idx.compressed(),index=df.index[~idx.mask],columns=['signal'])
     idx=source(idx.compressed(),m15.idx[~idx.mask])
     ts=pta.element(m15,idx) 
@@ -200,18 +181,9 @@ def main():
     a.merge_data('m1505',['2008-12-01 09:00:00','2008-12-28 23:00:00'],clms=s)
  
     t=np.array(a.data['m1505'].cl.values, dtype=float)
-    # import matplotlib.pyplot as plt
-    # plt.plot(t)
-    # plt.plot(talib.SMA(t,7))
-    # plt.plot(talib.SMA(t,20))
-    # plt.show()
-    # b=talib.SMA(t,7)-talib.SMA(t,20)
     b=stg(t)
     print(b)
-    # print(b)
     signal=cross(b)
-
-    # c=sma(b,n=5)
     print(len(b))
 
 
