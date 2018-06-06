66297d633b616dc8efb2048ffdbcf054577bbb6c
eel <eelraptor@163.com>
2018-06-06 21:17:45 +0800

    tst

--- reservior/tst.py
+++ reservior/tst.py
@@ -1,6 +1,8 @@
 import tushare as ts
 # print(type(ts.__dict__))
-for k,v in ts.__dict__.items():
-	print(k,v)
+# for k,v in ts.__dict__.items():
+# 	print(k,v)
 
-# print(ts.get_czce_daily(date='2017-01-05', type="future"))
+# print(ts.get_czce_daily(date='2018-06-06', type="future"))
+# print(ts.get_dce_daily(date='2018-06-06', type="future"))
+print(ts.get_shfe_daily(date='2018-06-06'))
