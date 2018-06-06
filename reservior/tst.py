4a7feeb3b5995fdf1bc44d5acfd09c303b12f8ba
eel <eelraptor@163.com>
2018-06-06 22:15:52 +0800

    tst

--- reservior/tst.py
+++ reservior/tst.py
@@ -2,7 +2,19 @@ import tushare as ts
 # print(type(ts.__dict__))
 # for k,v in ts.__dict__.items():
 # 	print(k,v)
+import pickle as pkl
 
-# print(ts.get_czce_daily(date='2018-06-06', type="future"))
+ 
+def save_var(v):
+   with open('var.pkl','wb') as fd:
+       pkl.dump(v,fd)
+
+def get_var():
+   with open('var.pkl','rb') as fd:
+       tp=pkl.load(fd)
+   return tp
+
+# save_var(ts.get_czce_daily(date='2018-06-06', type="future"))
+print(get_var())
 # print(ts.get_dce_daily(date='2018-06-06', type="future"))
-print(ts.get_shfe_daily(date='2018-06-06'))
+# print(ts.get_shfe_daily(date='2018-06-06'))
