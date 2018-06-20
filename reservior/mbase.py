99a2841557cc8e7ceff5db6f48cc4d9f6e76590a
alligatorme <alligatorme@gmail.com>
2018-06-20 22:29:25 +0800

    tst

--- reservior/mbase.py
+++ reservior/mbase.py
@@ -1,38 +1,47 @@
 import pickle as pkl
 import sqlite3, os
 
-def get_var():
-	with open('var.pkl','rb') as fd:
-		tp=pkl.load(fd)
-	return tp
-
 ROWS=['date','open','high','low','close','volume','open_interest','turnover','settle']
 
-ROWS_TYPE=['DATE','INT','INT','INT','INT','INTEGER','INTEGER','INTEGER','INT']
-ROWS_MARK=['?' for i in range(len(ROWS))]
+ROWS_TYPE=['DATE','REAL','REAL','REAL','REAL','INTEGER','INTEGER','INTEGER','REAL']
 COLUMN_DEFINE=', '.join(list(' '.join(i) for i in zip(ROWS,ROWS_TYPE)))
 
 class mbase():#metaclass=singleton
 	def __init__(self,fn):
 		if fn: self.base=sqlite3.connect(fn)
+		self.tables=self.table_list()
+
+	def table_list(self):
+		rst=self.base.execute("select name from sqlite_master where type='table' order by name")
+		return [i[0] for i in rst] 
 
 	def row_in(self,symbol,row):
-		sql="insert into %s (%s) values (%s)"%(symbol,','.join(ROWS),','.join(ROWS_MARK)) 
+		sql="insert into %s (%s) values (%s)"%(symbol,','.join(ROWS),','.join(['?' for i in range(len(ROWS))]))
 		self.base.execute(sql,row)
 
 	def create_table(self,symbol):
-		sql="create table if not exists %s (%s)"%(symbol,COLUMN_DEFINE+', PRIMARY KEY (date)')
-		self.base.execute(sql)
+		if symbol not in self.tables:
+			sql="create table if not exists %s (%s)"%(symbol,COLUMN_DEFINE+', PRIMARY KEY (date)')
+			self.base.execute(sql)
+			self.table.append(symbol)
+
+	def day_in(self,src):
+		for i in src.iterrows():
+			row=list(i[1])
+			self.base.create_table(row[0])
+			self.base.row_in(row[0],row[1:])
 
 	def leave(self):
 		self.base.close()
 
 if __name__=="__main__":
-	# print(ROWS)
-	# print(ROWS_MARK)
-	symbol='A1009'
-	sql="create table if not exists %s (%s)"%(symbol,COLUMN_DEFINE+', PRIMARY KEY (date)')
-	print(sql)
-#	sd=mbase('temp.db')
-#	sd.create_table(symbol)
-#	sd.leave()
+	sd=mbase('temp.db')
+	import pandas as pd
+	from tst import get_var
+#		sd.leave()
+#	day_in('',get_var())
+	ab=['symbol']
+	ab.extend(ROWS)
+#	sd.day_in(get_var()[ab])
+	print(sd.table_list())
+	sd.leave()
--- reservior/tst.py
+++ reservior/tst.py
@@ -4,16 +4,16 @@ import pickle as pkl
 import datetime
 # print(type(ts.__dict__))
 # for k,v in ts.__dict__.items():
-# 	print(k,v)
+#	print(k,v)
 
 def save_var(v):
-   with open('var.pkl','wb') as fd:
-       pkl.dump(v,fd)
+	with open('var.pkl','wb') as fd:
+	   pkl.dump(v,fd)
 
 def get_var():
-   with open('var.pkl','rb') as fd:
-       tp=pkl.load(fd)
-   return tp
+	with open('var.pkl','rb') as fd:
+	   tp=pkl.load(fd)
+	return tp
 
 #save_var(ts.get_dce_daily(date='2018-06-06', type="future"))
 #print(get_var())
@@ -22,11 +22,11 @@ def get_var():
 
 
 # def serial_date(begin=None,end=datetime.datetime(2000, 1, 1)):
-# 	day=begin or datetime.datetime.now()
-# 	while day>=end:
-# 		if day.weekday()-5<0:
-# 			yield day.strftime('%Y-%m-%d')
-# 		day=day-datetime.timedelta(days=1)
+#	day=begin or datetime.datetime.now()
+#	while day>=end:
+#		if day.weekday()-5<0:
+#			yield day.strftime('%Y-%m-%d')
+#		day=day-datetime.timedelta(days=1)
 
 # x=serial_date(end=datetime.datetime(2018, 5, 2))
 # for i in x: print(i)
@@ -40,4 +40,6 @@ def get_raw(day='2016-06-22'):
 		func=ts.__dict__['get_'+k+'_daily']
 		print(k,'----')
 		print(func(**factor).head())
-get_raw()
+
+if __name__=="__main__":
+	get_raw()
