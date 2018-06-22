a7515e704e807ff3753737067726030c01d1be6e
alligatorme <alligatorme@gmail.com>
2018-06-22 22:35:01 +0800

    tst

--- reservior/mbase.py
+++ reservior/mbase.py
@@ -1,5 +1,15 @@
-import pickle as pkl
 import sqlite3, os
+import taken
+import pandas as pd
+import numpy as np
+
+def row_new(dst,src):
+	if not np.any(src-dst):return false
+#		if np.all((src[1:]-rst)>=0):
+
+def row_valid(src):
+	if isinstance(src,list):src=np.array(src)
+	return np.all(src>=0)
 
 ROWS=['date','open','high','low','close','volume','open_interest','turnover','settle']
 
@@ -9,39 +19,70 @@ COLUMN_DEFINE=', '.join(list(' '.join(i) for i in zip(ROWS,ROWS_TYPE)))
 class mbase():#metaclass=singleton
 	def __init__(self,fn):
 		if fn: self.base=sqlite3.connect(fn)
-		self.tables=self.table_list()
+		self.get_tables()
 
-	def table_list(self):
+	def get_tables(self):
 		rst=self.base.execute("select name from sqlite_master where type='table' order by name")
-		return [i[0] for i in rst] 
+		self.tables=[i[0] for i in rst]
+
+	def create_table(self,symbol):
+		if not symbol in self.tables:
+			print(symbol)
+			sql="create table if not exists %s (%s)"%(symbol,COLUMN_DEFINE+', PRIMARY KEY (date)')
+			self.base.execute(sql)
+			self.tables.append(symbol)
 
 	def row_in(self,symbol,row):
 		sql="insert into %s (%s) values (%s)"%(symbol,','.join(ROWS),','.join(['?' for i in range(len(ROWS))]))
 		self.base.execute(sql,row)
 
-	def create_table(self,symbol):
-		if symbol not in self.tables:
-			sql="create table if not exists %s (%s)"%(symbol,COLUMN_DEFINE+', PRIMARY KEY (date)')
-			self.base.execute(sql)
-			self.table.append(symbol)
-
 	def day_in(self,src):
 		for i in src.iterrows():
 			row=list(i[1])
-			self.base.create_table(row[0])
-			self.base.row_in(row[0],row[1:])
+			self.create_table(row[0])
+			self.row_in(row[0],row[1:])
+
+	def row_update(self,src):
+#		symbol=src[0]
+#		date=src[1]
+		col=lambda x:','.join(x)
+		sql="select %s from %s where date=%s"%(col(ROWS[1:]),src[0],src[1])
+		rst=self.base.execute(sql).fetchone()
+		if row_new(list(rst),src[1:]):
+			sql="replace into %s (%s) values (%s)"%(src[0],col(ROWS),','.join(['?' for i in range(len(col(ROWS)))]))
+			self.base.execute(sql,src[1:])
+			return true
+		else:
+			return false
 
 	def leave(self):
+		self.base.commit()
 		self.base.close()
+
+	def clean_table(self):
+		for i in self.tables:
+			self.base.execute("drop table %s"%(i))
+		self.base.execute("vacuum")
+
+def main02():
+	sd.row_update(['A1901','20180621'])
+#	print(rst)
+
+def main01():
+#	sd.clean_table()
+#	sd.get_tables()
+	tk=taken.market()
+	ab=['symbol']
+	ab.extend(ROWS)
+	for i in taken.serial_date(end='2018-06-01'):
+		print(i)
+		for j in tk.raw_daily(i):
+			if isinstance(j,pd.core.frame.DataFrame):
+				sd.day_in(j[ab])
+#				print(j.head(1))
 
 if __name__=="__main__":
 	sd=mbase('temp.db')
-	import pandas as pd
-	from tst import get_var
-#		sd.leave()
-#	day_in('',get_var())
-	ab=['symbol']
-	ab.extend(ROWS)
-#	sd.day_in(get_var()[ab])
-	print(sd.table_list())
+	main02()
 	sd.leave()
+
---
+++ reservior/taken.py
@@ -0,0 +1,36 @@
+import tushare as ts
+import datetime
+
+import pickle as pkl
+def save_var(v):
+	with open('var.pkl','wb') as fd:
+		pkl.dump(v,fd)
+
+def get_var():
+	with open('var.pkl','rb') as fd:
+		tp=pkl.load(fd)
+	return tp
+
+def serial_date(end,begin=None):
+	day=begin and datetime.datetime.strptime(begin,'%Y-%m-%d') or datetime.datetime.now()
+	end=datetime.datetime.strptime(end,'%Y-%m-%d')
+	while day>=end:
+		if day.weekday()-5<0:
+			yield day.strftime('%Y-%m-%d')
+		day-=datetime.timedelta(days=1)
+
+class market():
+	def __init__(self):
+		self.src={'dce':'future','shfe':None,'czce':'future'}
+
+	def raw_daily(self,day):
+		for k,v in self.src.items():
+			factor={}
+			factor['date']=day
+			if v: factor['type']=v
+			func=ts.__dict__['get_'+k+'_daily']
+			yield func(**factor)
+
+if __name__=="__main__":
+	for i in serial_date():
+		print(i)
