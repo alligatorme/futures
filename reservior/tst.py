e149967a4746b18fc215b4c3a3021f6cac469269
alligatorme <alligatorme@gmail.com>
2018-06-29 12:09:50 +0800

    tst

--- reservior/tst.py
+++ reservior/tst.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import mbase,taken
+import pickle as pkl
+def save_var(n,v):
+	with open(n,'wb') as fd:
+		pkl.dump(v,fd)
 
 def main03():
 	pass
@@ -14,15 +18,17 @@ def main02():
 
 def main01():
 #	sd.clean_table()
-	sd.get_tables()
+#	sd.get_tables()
 	tk=taken.market()
 	ab=['symbol']
 	ab.extend(mbase.ROWS)
 	for i in taken.serial_date(begin='2018-06-27',end='2008-01-01'):
 		print(i)
+		rst=[]
 		for j in tk.raw_daily(i):
-			if isinstance(j,pd.core.frame.DataFrame):
-				sd.day_in(j[ab])
+			if isinstance(j,pd.core.frame.DataFrame):rst.append(j)
+		if rst:save_var(i,pd.concat(rst))
+#				sd.day_in(j[ab])
 #				print(j.head(1))
 #				for i in j.iterrows():
 #					print(list(i[1])[0],end='\t')
